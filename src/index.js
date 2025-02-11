4c0215704a898b748822203b5b18b44e0e903116
Andrey Lebedev <alebedev424@gmail.com>
2025-02-11 11:44:08 +0300

    refactor: переделал бегущую строку на список

--- src/index.js
+++ src/index.js
@@ -60,8 +60,8 @@ const stagesCarousel = new StagesCarousel(
 
 carouselLeftButton.addEventListener("click", () => {
   stagesCarousel.backward();
-
 });
 carouselRightButton.addEventListener("click", () => {
   stagesCarousel.forward();
 });
+
